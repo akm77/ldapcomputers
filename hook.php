e6bed3ebb3b9657a4c88d37f39937fa8ae0302d3
Aleksey Kotryakhov <Aleksey.Kotryakhov@gmail.com>
2019-10-23 12:07:43 +1000

    Plugin hooks

--- hook.php
+++ hook.php
@@ -32,6 +32,31 @@
  * @return boolean
  */
 function plugin_ldapcomputers_install() {
+   global $DB;
+
+   $migration = new Migration(PLUGIN_BARCODE_VERSION);
+
+   //Create config table only if it does not exists yet!
+   if (!$DB->tableExists('glpi_plugin_ldapcomputers_configs')) {
+      $query = '';
+      $DB->queryOrDie($query, $DB->error());
+   }
+
+   //Create computers table only if it does not exists yet!
+   if (!$DB->tableExists('glpi_plugin_ldapcomputers_computers')) {
+      $query = '';
+      $DB->queryOrDie($query, $DB->error());
+   }
+   /* Placeholder for further update process in future
+   if (!$DB->tableExists('glpi_plugin_ldapcomputers_configs')) {
+      $query = '';
+      $DB->queryOrDie($query, $DB->error());
+   }
+   */
+
+   //execute the whole migration
+   $migration->executeMigration();
+
    return true;
 }
 
@@ -41,5 +66,22 @@ function plugin_ldapcomputers_install() {
  * @return boolean
  */
 function plugin_ldapcomputers_uninstall() {
+   global $DB;
+
+   $tables = [
+      'configs',
+      'computers'
+   ];
+
+   foreach ($tables as $table) {
+      $tablename = 'glpi_plugin_ldapcomputers_' . $table;
+      //Drop table only if it does not exists yet!
+      if ($DB->tableExists($tablename)) {
+         $DB->queryOrDie(
+            "DROP TABLE `$tablename`",
+            $DB->error()
+         );
+      }
+   }
    return true;
 }
