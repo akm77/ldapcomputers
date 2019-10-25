27647d6a3a02e6a6b86396e0878d7c8d4266591a
Aleksey Kotryakhov <Aleksey.Kotryakhov@gmail.com>
2019-10-25 16:46:17 +1000

    Config

---
+++ front/config.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ -------------------------------------------------------------------------
+ LDAP computers plugin for GLPI
+ Copyright (C) 2019 by the ldapcomputers Development Team.
+
+ https://github.com/pluginsGLPI/ldapcomputers
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of LDAP computers.
+
+ LDAP computers is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ LDAP computers is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with LDAP computers. If not, see <http://www.gnu.org/licenses/>.
+
+------------------------------------------------------------------------
+
+   @package   Plugin LDAP Computers
+   @author    Aleksey Kotryakhov
+   @co-author
+   @copyright Copyright (c) 2009-2016 Barcode plugin Development team
+   @license   AGPL License 3.0 or (at your option) any later version
+              http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   @link      https://github.com/akm77/ldapcomputers
+   @since     2019
+
+
+ --------------------------------------------------------------------------
+ */
+include ('../../../inc/includes.php');
+
+// Check if plugin is activated...
+$plugin = new Plugin();
+
+if (!$plugin->isInstalled('ldapcomputers') || !$plugin->isActivated('ldapcomputers')) {
+   Html::displayNotFoundError();
+}
+
+//check for ACLs
+   //Add page header
+   Html::header(
+     __('My example plugin', 'myexampleplugin'),
+     $_SERVER['PHP_SELF'],
+     'assets',
+     'pluginmyexamplemyobject',
+     'myobject'
+   );
+
+   Search::show('PluginLdapcomputersConfig');
+
+   Html::footer();
