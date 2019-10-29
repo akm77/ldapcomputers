8193c35610f6bf73eb30145caedc676667670dd8
Aleksey Kotryakhov <Aleksey.Kotryakhov@gmail.com>
2019-10-29 13:33:12 +1000

    Config form

--- front/config.form.php
+++ front/config.form.php
@@ -38,3 +38,84 @@
 
  --------------------------------------------------------------------------
  */
+include ('../../../inc/includes.php');
+
+Session::checkRight("plugin_ldapcomputers", UPDATE);
+
+$config_ldap = new PluginLdapcomputersConfig();
+
+if (!isset($_GET['id'])) {
+   $_GET['id'] = "";
+}
+//LDAP Server add/update/delete
+if (isset($_POST["update"])) {
+   $config_ldap->update($_POST);
+   Html::back();
+
+} else if (isset($_POST["add"])) {
+   //If no name has been given to this configuration, then go back to the page without adding
+   if ($_POST["name"] != "") {
+      if ($newID = $config_ldap->add($_POST)) {
+         if (PluginLdapcomputersConfig::testLDAPConnection($newID)) {
+            Session::addMessageAfterRedirect(__('Test successful'));
+         } else {
+            Session::addMessageAfterRedirect(__('Test failed'), false, ERROR);
+            GlpiNetwork::addErrorMessageAfterRedirect();
+         }
+         Html::redirect($CFG_GLPI["root_doc"] . "/front/authldap.php?next=extauth_ldap&id=".$newID);
+      }
+   }
+   Html::back();
+
+} else if (isset($_POST["purge"])) {
+   $config_ldap->delete($_POST, 1);
+   $_SESSION['glpi_authconfig'] = 1;
+   $config_ldap->redirectToList();
+
+} else if (isset($_POST["test_ldap"])) {
+   $config_ldap->getFromDB($_POST["id"]);
+
+   if (PluginLdapcomputersConfig::testLDAPConnection($_POST["id"])) {
+                                       //TRANS: %s is the description of the test
+      $_SESSION["LDAP_TEST_MESSAGE"] = sprintf(__('Test successful: %s'),
+                                               //TRANS: %s is the name of the LDAP main server
+                                               sprintf(__('Main server %s'), $config_ldap->fields["name"]));
+   } else {
+                                       //TRANS: %s is the description of the test
+      $_SESSION["LDAP_TEST_MESSAGE"] = sprintf(__('Test failed: %s'),
+                                               //TRANS: %s is the name of the LDAP main server
+                                               sprintf(__('Main server %s'), $config_ldap->fields["name"]));
+      GLPINetwork::addErrorMessageAfterRedirect();
+   }
+   Html::back();
+
+} else if (isset($_POST["test_ldap_replicate"])) {
+   $replicate = new PluginLdapcomputersConfigbackupldap();
+   $replicate->getFromDB($_POST["ldap_replicate_id"]);
+
+   if (PluginLdapcomputersConfig::testLDAPConnection($_POST["id"], $_POST["ldap_replicate_id"])) {
+                                       //TRANS: %s is the description of the test
+      $_SESSION["LDAP_TEST_MESSAGE"] = sprintf(__('Test successful: %s'),
+                                               //TRANS: %s is the name of the LDAP replica server
+                                               sprintf(__('Replicate %s'), $replicate->fields["name"]));
+   } else {
+                                        //TRANS: %s is the description of the test
+      $_SESSION["LDAP_TEST_MESSAGE"] = sprintf(__('Test failed: %s'),
+                                               //TRANS: %s is the name of the LDAP replica server
+                                               sprintf(__('Replicate %s'), $replicate->fields["name"]));
+      GLPINetwork::addErrorMessageAfterRedirect();
+   }
+   Html::back();
+
+} else if (isset($_POST["add_replicate"])) {
+   $replicate = new PluginLdapcomputersConfigbackupldap();
+   unset($_POST["next"]);
+   unset($_POST["id"]);
+   $replicate->add($_POST);
+   Html::back();
+}
+
+Html::header(PluginLdapcomputersConfig::getTypeName(1), $_SERVER['PHP_SELF'], 'config', 'auth', 'ldap');
+$config_ldap->display($_GET);
+
+Html::footer();
