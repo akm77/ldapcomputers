c203601e903ddb30cb082c765daf158e0690c970
Aleksey Kotryakhov <Aleksey.Kotryakhov@gmail.com>
2019-11-13 14:51:55 +1000

    Computer class

--- inc/computer.class.php
+++ inc/computer.class.php
@@ -200,5 +200,98 @@ class PluginLdapcomputersComputer extends CommonDBTM {
       ];
       return $tab;
    }
+   /**
+    * Show import computers form
+    *
+    * @param object $ldap_server PluginLdapcomputersConfig object
+    *
+    * @return void
+    */
+   static function showComputersImportForm(PluginLdapcomputersConfig $ldap_server) {
+
+      //Get data related to entity (directory and ldap filter)
+      $ldap_server->getFromDB($_SESSION['ldap_computers_import']['primary_ldap_id'] );
+      echo "<div class='center'>";
+
+      echo "<form method='post' action='".$_SERVER['PHP_SELF']."'>";
+
+      echo "<table class='tab_cadre_fixe'>";
+
+      echo "<tr><th colspan='4' class='middle'><div class='relative'>";
+      echo "<span>" . __('Import computers', 'ldapcomputers');
+
+      echo "</span></div>";
+      echo "</th></tr>";
+
+      if (PluginLdapcomputersConfig::getNumberOfServers() > 1) {
+         $rand = mt_rand();
+         echo "<tr class='tab_bg_2'><td><label for='dropdown_primary_ldap_id$rand'>".__('LDAP directory choice')."</label></td>";
+         echo "<td colspan='3'>";
+            PluginLdapcomputersConfig::dropdown(['name'                 => 'primary_ldap_id',
+                            'value'                => $_SESSION['ldap_computers_import']['primary_ldap_id'],
+                            'condition'            => ['is_active' => 1],
+                            'display_emptychoice'  => false,
+                            'rand'                 => $rand]);
+         echo "&nbsp;<input class='submit' type='submit' name='change_directory'
+                  value=\""._sx('button', 'Change')."\">";
+         echo "</td></tr>";
+      }
+      //If multi-entity mode and more than one entity visible
+      //else no need to select entity
+      if (Session::isMultiEntitiesMode()
+         && (count($_SESSION['glpiactiveentities']) > 1)) {
+
+         echo "<tr class='tab_bg_2'><td>".__('Select the desired entity')."</td>".
+                 "<td colspan='3'>";
+         Entity::dropdown(['value'       => $_SESSION['ldap_computers_import']['entities_id'],
+                           'entity'      => $_SESSION['glpiactiveentities'],
+                           'on_change'    => 'submit()']);
+         echo "</td></tr>";
+      } else {
+            //Only one entity is active, store it
+         echo "<tr><td><input type='hidden' name='entities_id' value='".
+                        $_SESSION['glpiactive_entity']."'></td></tr>";
+      }
+      if (($_SESSION['ldap_computers_import']['primary_ldap_id'] !=  NOT_AVAILABLE)
+         && ($_SESSION['ldap_computers_import']['primary_ldap_id'] > 0)) {
+         if ($_SESSION['ldap_computers_import']['primary_ldap_id']) {
+            echo "<tr class='tab_bg_2'><td colspan='4' class='center'>";
+            echo "<input class='submit' type='submit' name='search' value=\"".
+                _sx('button', 'Search')."\">";
+            echo "</td></tr>";
+         } else {
+            echo "<tr class='tab_bg_2'><".
+                 "td colspan='4' class='center'>".__('No directory selected')."</td></tr>";
+         }
+      } else {
+         echo "<tr class='tab_bg_2'><td colspan='4' class='center'>".
+                __('No directory associated to entity: impossible search')."</td></tr>";
+      }
+      echo "</table>";
+      Html::closeForm();
+      echo "</div>";
+   }
+
+   /**
+    * Search user
+    *
+    * @param resource $authldap AuthLDAP object
+    *
+    * @return void
+    */
+   static function searchComputers(PluginLdapcomputersConfig $ldap_server) {
+
+      if (PluginLdapcomputersLdap::connectToServer($ldap_server->getField('host'), $ldap_server->getField('port'),
+                                $ldap_server->getField('rootdn'),
+                                Toolbox::decrypt($ldap_server->getField('rootdn_passwd'), GLPIKEY),
+                                $ldap_server->getField('use_tls'),
+                                $ldap_server->getField('deref_option'))) {
+         self::showLdapUsers();
+
+      } else {
+         echo "<div class='center b firstbloc'>".__('Unable to connect to the LDAP directory');
+      }
+   }
+
 
 }
