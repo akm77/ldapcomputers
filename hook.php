b2ce9b3742b04b4d926fc7f445c292140f3bf3db
Aleksey Kotryakhov <Aleksey.Kotryakhov@gmail.com>
2020-01-22 11:40:34 +1000

    Hook

--- hook.php
+++ hook.php
@@ -176,6 +176,64 @@ function plugin_ldapcomputers_getDatabaseRelations() {
       return [];
    }
 }
+////// SEARCH FUNCTIONS ///////(){
+
+// Define search option for types of the plugins
+function plugin_ldapcomputers_getAddSearchOptions($itemtype) {
+   $sopt = [];
+   if ($itemtype != 'Computer') {
+      return $sopt;
+   }
+
+   $plugin = new Plugin();
+
+   if ($plugin->isInstalled('ldapcomputers')
+       && $plugin->isActivated('ldapcomputers')
+       && Session::haveRight("plugin_ldapcomputers_view", READ)) {
+        $sopt[3210]['table']         = 'glpi_plugin_ldapcomputers_computers';
+        $sopt[3210]['field']         = 'name';
+        $sopt[3210]['linkfield']     = '';
+        $sopt[3210]['name']          = "LDAP " . __("Name");
+        $sopt[3210]['joinparams']  = ['jointype' => 'child'];
+        $sopt[3210]['forcegroupby']  = true;
+
+        $sopt[3211]['table']         = 'glpi_plugin_ldapcomputers_computers';
+        $sopt[3211]['field']         = 'lastLogon';
+        $sopt[3211]['linkfield']     = '';
+        $sopt[3211]['name']          = "LDAP " . __('Last logon', 'ldapcomputers');
+        $sopt[3211]['joinparams']  = ['jointype' => 'child'];
+        $sopt[3211]['forcegroupby']  = true;
+        $sopt[3211]['datatype']      = 'datetime';
+
+        $sopt[3212]['table']         = 'glpi_plugin_ldapcomputers_computers';
+        $sopt[3212]['field']         = 'distinguishedName';
+        $sopt[3212]['linkfield']     = '';
+        $sopt[3212]['name']          = "LDAP " . __('Distinguished name', 'ldapcomputers');
+        $sopt[3212]['joinparams']  = ['jointype' => 'child'];
+        $sopt[3212]['forcegroupby']  = true;
+
+        $sopt[3213]['table']         = 'glpi_plugin_ldapcomputers_computers';
+        $sopt[3213]['field']         = 'operatingSystem';
+        $sopt[3213]['linkfield']     = '';
+        $sopt[3213]['name']          = "LDAP " . __('OS', 'ldapcomputers');
+        $sopt[3213]['joinparams']  = ['jointype' => 'child'];
+        $sopt[3213]['forcegroupby']  = true;
+   }
+   return $sopt;
+}
+
+function plugin_ldapcomputers_addLeftJoin($type, $ref_table, $new_table, $linkfield, &$already_link_tables) {
+   $out = "";
+   switch ($new_table) {
+      case "glpi_plugin_ldapcomputers_computers": // From order list
+         $out = " LEFT JOIN `glpi_plugin_ldapcomputers_computers`
+                     ON `glpi_plugin_ldapcomputers_computers`.`name` = `glpi_computers`.`name` ";
+         break;
+   }
+
+   return $out;
+}
+
 
 /**
  * Plugin uninstall process
